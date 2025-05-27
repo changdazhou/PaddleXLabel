dd17f7086b635e72f9062ad733b580106691c138
changdazhou <changda1650@163.com>
2025-05-27 02:58:18 +0000

    v1

--- app/models.py
+++ app/models.py
@@ -1,7 +1,21 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import UserMixin
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-class User():
+from werkzeug.security import check_password_hash
+
+
+class User:
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
@@ -19,6 +33,6 @@ class User():
 
     def get_id(self):
         return str(self.id)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
